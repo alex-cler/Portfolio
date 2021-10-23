9ddf0d01ea7b1f4677217eae21c46458b78aad12
Alex <alex28.cler@gmail.com>
2021-10-23 23:21:34 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -11,14 +11,14 @@ function delay(n) {
 function pageTransition() {
     var tl = gsap.timeline();
     tl.to(".loading-screen", {
-        duration: 1.2,
+        duration: 1,
         width: "100%",
         left: "0%",
         ease: "Expo.easeInOut",
     });
 
     tl.to(".loading-screen", {
-        duration: 1,
+        duration: 0.8,
         width: "100%",
         left: "100%",
         ease: "Expo.easeInOut",
@@ -29,7 +29,7 @@ function pageTransition() {
 
 function contentAnimation() {
     var tl = gsap.timeline();
-    tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2 });
+    tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.1 });
 }
 
 $(function () {
